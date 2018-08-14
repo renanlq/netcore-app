781d6c0ca3b9b486c180299d93a37d3f87f2e4fa
Renan Lemes Queiroz <renan@MacBook-Pro-de-Renan.local>
2018-08-14 00:24:32 -0300

    add json config file, and configurationbuilder to read it.

--- section2/module2/hosting/Program.cs
+++ section2/module2/hosting/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 
 namespace hosting
 {
@@ -12,6 +11,7 @@ namespace hosting
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseStartup<Startup>()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .Build();
 
             host.Run();
--- section2/module2/hosting/Startup.cs
+++ section2/module2/hosting/Startup.cs
@@ -1,20 +1,40 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace hosting
 {
     public class Startup
     {
+        private IConfigurationRoot _configuration;
+
+        public Startup (IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("application.json");
+
+            builder.AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+        }
+
         public void Configure(IApplicationBuilder app)
         {
+            // 1
             // app.Use(async (context, next) => {
             //     context.Response.Headers.Add("Middleware", "MiddlewareNetCore");
             //     await next.Invoke();
             // });
 
-            app.UseMiddleware<MyMiddleware>();
+            // 2
+            // app.UseMiddleware<MyMiddleware>();
 
-            app.Run(context => context.Response.WriteAsync("Ola mundo 2 | "));
+            // 3
+            var applicationName = _configuration.GetValue<string>("ApplicationName");
+            
+            app.Run(context => context.Response.WriteAsync($"Ola mundo, modulo 2: {applicationName}."));
         }
     }
 }
