5742ea366f615b2adca965208498286117e410bc
Renan Lemes Queiroz <renan@MacBook-Pro-de-Renan.local>
2018-09-12 00:53:44 -0300

    add conn and depedencies between projects

--- site/src/StoreOfBuild.DI/Bootstrap.cs
+++ site/src/StoreOfBuild.DI/Bootstrap.cs
@@ -1,12 +1,15 @@
 using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StoreOfBuild.Data;
 
 namespace StoreOfBuild.DI
 {
     public class Bootstrap
     {
-        public static void Configure()
+        public static void Configure(IServiceCollection services, string conn)
         {
-            
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(conn));
         }
     }
 }
